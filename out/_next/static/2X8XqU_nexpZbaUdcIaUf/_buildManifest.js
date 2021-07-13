8be5c8d76e7bb22e2d4932a2ead0a93c3ec8302c
osrs-keepa <69741747+osrs-keepa@users.noreply.github.com>
2021-07-12 20:53:38 -0500

    Delete out directory

--- out/_next/static/2X8XqU_nexpZbaUdcIaUf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/51b81a57ddfd0f674e99.css","static/chunks/pages/index-3a4916adf7765c97a24e.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
