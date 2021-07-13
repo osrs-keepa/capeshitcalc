cdc30eb6ebc06e44e3103d656de5e9c210001bee
osrs-keepa <69741747+osrs-keepa@users.noreply.github.com>
2021-07-13 12:05:12 -0500

    Add files via upload

---
+++ _next/static/nzfBcznzIYYqvQxl6eMnp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d40f678f765793a34749.css","static/chunks/pages/index-5b19a9b406e90107905d.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
